2eb3bba717bc884375a4b4e98b426caede70ab2d
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2026-05-17 12:51:34 -0400

    Set up basic routing.
    In index.js, require the filepath
    module at the top.
    
    In a resolveRoute function,
    set up an object lookup map.
    The function takes in
    a basePath and a requestUrl,
    and looks up the respective
    file
    in the lookup map (depending on
    the requestUrl). or 404
    (if the requestUrl does not
    exist in the lookup map).
    Set a status accordingly
    (depends on if the requestUrl
    is a key in the obect).
    
    Return the filePath and the
    status as an object
    
    Pass the views directory and
    the req.url to this function
    inside
    createServer and destructure
    filePath and status.
    
    Use the fs readFile module
    to read the
    file from filePath, log an
    error is there is one or
    send the file and the statusCode
    as a response to the browser.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const http = require("http");
+const fs = require("fs");
+
+const resolveRoute = (basePath, requestUrl) => {
+  const fileMap = {
+    "/": "index.html",
+    "/about": "about.html",
+    "/contact-me": "contact-me.html",
+  };
+
+  const fileName = fileMap[requestUrl] || "/404.html";
+
+  const status = Object.hasOwn(fileMap, requestUrl) ? 200 : 404;
+
+  return { filePath: basePath + fileName, status };
+};
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  res.setHeader("Content-Type", "text/html");
+
+  const { filePath, status } = resolveRoute("./views/", req.url);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.statusCode = status;
+      res.end(data);
+    }
+  });
 });
 
 server.listen(8080, () => {
